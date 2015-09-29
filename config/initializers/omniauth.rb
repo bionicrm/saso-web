718d3ddc7fe0840714b0eca71463e33b3a56c15f
T.J. Mazeika <t@mazeika.me>
2015-09-28 23:50:07 -0400

    Add DigitalOcean OAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+
   # GitHub
   provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'],
            scope: 'user:email'
@@ -10,4 +11,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # Twitter
   provider :twitter, ENV['TWITTER_CLIENT_ID'], ENV['TWITTER_CLIENT_SECRET'],
            secure_image_url: true, x_auth_access_type: 'read'
+
+  # DigitalOcean
+  provider :digitalocean, ENV['DIG_OC_CLIENT_ID'], ENV['DIG_OC_CLIENT_SECRET'],
+           scope: 'read'
+
 end
